157f0c491a301fda5be99a2f2b6f436d12178211
Alex Dikelsky <alexander.dikelsky@gmail.com>
2021-09-19 21:55:13 -0500

    Work a bit more on paths

--- src/path.ts
+++ src/path.ts
@@ -1,10 +1,11 @@
 type ListNode = [contents: String, toRightCost: number]
+type OutPath = [leftPath: ListNode[], rightPath: ListNode[]]
 
 const upperFloor: ListNode[] = [
-    ["A", 1],
+    ["Entry", 1],
     ["B", 5],
     ["C", 8],
-    ["D", 15],
+    ["D", 700],
     ["E", 3],
 ]
 
@@ -16,21 +17,38 @@ function filterUnneeded(list: ListNode[], toUse: String[]): ListNode[] {
 //  If it isn't in the toUse list, preserve distance by adding to the old path
 function addDistance(prevPath: ListNode[], currentNode: ListNode, toUse: String[]): ListNode[] {
     let hasStr = (node: ListNode) => 
-        (typeof toUse.find(s => s === node[0])) === "undefined";
+        (typeof toUse.find(s => s === node[0])) !== "undefined";
     if (hasStr(currentNode)) {
+        return prevPath.concat([currentNode]);
+    } else {
         let lastIndex = prevPath.length - 1;
         let last = prevPath[lastIndex];
         let newLast: ListNode = [last[0], last[1] + currentNode[1]];
-        return prevPath.slice(0, lastIndex - 1).concat([newLast]);
-    } else {
-        return prevPath.concat([currentNode]);
+        return prevPath.slice(0, lastIndex).concat([newLast]);
     }
 }
 
 // Filter out unused nodes without changing the route length
 function filterPreserveLength(list: ListNode[], toUse: String[]): ListNode[] {
     let addDistWithLocations = (list: ListNode[], currentNode: ListNode) => addDistance(list, currentNode, toUse);
-    return list.reduce(addDistWithLocations, [["Entry", 0]])
+    return list.slice(1).reduce(addDistWithLocations, [list[0]])
 }
 
-console.log(filterPreserveLength(upperFloor, ["A", "E"]));``
+// Precondition: list is filtered so we need to visit every node
+// Go left, then go right
+// Using brute force now
+function shortestPath(list: ListNode[]): OutPath {
+    
+    let ltrSum = (path: ListNode[]) => path.reduce((previous, current) => previous + current[1], 0);
+    let rtlSum = (path: ListNode[]) => ltrSum(path.reverse());
+
+    let costWithPivot = Array.from(list.keys()).map(i => {
+        console.log(list.slice(0, i));
+        console.log(list.slice(i));
+        return [ltrSum(list.slice(i)) + rtlSum(list.slice(0, list.length - i)), i]
+    });
+    
+    console.log(costWithPivot)
+}
+
+console.log(shortestPath(upperFloor));
