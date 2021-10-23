fea6071c7ae093933c7afe8853d8504adad7adfb
Isaac List <listis01@luther.edu>
2021-10-23 15:00:30 -0500

    restructure app for clarity, add license header

--- index.js
+++ index.js
@@ -1,58 +1,73 @@
-const cool = require('cool-ascii-faces');
-const express = require('express');
-const path = require('path');
-const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-const PORT = process.env.PORT || 5000;
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
-
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-
-  .get('/cool', (req, res) => res.send(cool()))
-  .get('/db', async (req, res) => {
-      try {
-        const client = await pool.connect();
-        const result = await client.query('SELECT * FROM test_table');
-        const results = { 'results': (result) ? result.rows : null};
-        res.render('pages/db', results );
-        client.release();
-      } catch (err) {
-        console.error(err);
-        res.send("Error " + err);
-      }
-    })
-  .get('/classify', (req, res) => res.render('pages/classification', classify()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
-function classify() {
-  // Get ISBN provided by user
-  // let searchbox = document.getElementById("isbn");
-  // let isbn = searchbox.value;
-
-  let isbn = 9781101972083;
-
-  const request = new XMLHttpRequest();
-  let baseURL = "http://classify.oclc.org/classify2/Classify?";
-
-  let url = baseURL + "isbn=" + isbn + "&summary=true";
-
-  request.open("GET", url);
-  request.send();
-
-  request.onload = (e) => {
-    console.log(request.response);
-    resp = request.response;
-  }
-
-  return resp;
-}
+ const express = require('express');
+ const app = express();
+ 
+ const cool = require('cool-ascii-faces');
+ const path = require('path');
+ const PORT = process.env.PORT || 3000;
+ 
+ const { Pool } = require('pg');
+ const pool = new Pool({
+   connectionString: process.env.DATABASE_URL,
+   ssl: {
+     rejectUnauthorized: false
+   }
+ });
+ 
+ // Invoke listen method
+ app.listen(PORT, () => {
+   console.log(`app listening on port ${PORT}`);
+ });
+ 
+ // Set the path for web page source files and ejs engine
+ app.set('views', path.join(__dirname, 'views'));
+ app.set('view engine', 'ejs');
+ 
+ // Define the application routes
+ app.get('/', (req, res) => res.render('pages/index'));
+ app.get('/cool', (req, res) => res.send(cool()));
+ app.get('/db', async (req, res) => {
+   try {
+     const client = await pool.connect();
+     const result = await client.query('SELECT * FROM test_table');
+     const results = { 'results': (result) ? result.rows : null};
+     res.render('pages/db', results );
+     client.release();
+   } catch (err) {
+     console.error(err);
+     res.send("Error " + err);
+   }
+ });
+ app.get('/classify', (req, res) => res.render('pages/classification'));
+ 
+ // 404 Route
+ // TODO: Implement a 404 page
+ app.use((req, res) => res.status(404).render('pages/404'));
+ 
+ function classify() {
+   // Get ISBN provided by user
+   // let searchbox = document.getElementById("isbn");
+   // let isbn = searchbox.value;
+ 
+   let isbn = 9781101972083;
+ 
+   const request = new XMLHttpRequest();
+   let baseURL = "http://classify.oclc.org/classify2/Classify?";
+ 
+   let url = baseURL + "isbn=" + isbn + "&summary=true";
+ 
+   request.open("GET", url);
+   request.send();
+ 
+   request.onload = (e) => {
+     console.log(request.response);
+     resp = request.response;
+   }
+ 
+   return resp;
+ }
