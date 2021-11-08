8fd6c125652009eb8dc87a7d0c27975cf92fcde3
Isaac List <listis01@luther.edu>
2021-11-08 16:24:26 -0600

    add record of creating booklist table

---
+++ docs/setup/psql-setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Enter heroku's psql shell
+heroku pg:psql
+
+# Re-Create tables
+create table floorspreus ( shelves json NOT NULL, floor int NOT NULL, mincode varchar(8) NOT NULL ) ;
+
+create table booklist IF NOT EXISTS (
+    isbn INT PRIMARY KEY,
+    author VARCHAR(50),
+    title VARCHAR(150),
+    pub_year INT
+);
