c83f849d0ea84b7ee3d093c5a3558d635c48006b
Alex Dikelsky <alexander.dikelsky@gmail.com>
2021-11-16 14:26:32 -0600

    Working on the distance calculations

--- a.js
+++ a.js
@@ -1,13 +1,37 @@
 const lc = require('lc_call_number_compare');
 
-function skip_shelves(path, lower, booklocs) {
+// path: json from the database for a particular floor
+// lower: lower bound (call number)
+// booklocs: List of call numbers
+//
+// No mutation
+function keep_shelves_indexes(path, lower, booklocs) {
+    let result = [];
     for (let i = 0; i < path.length; i++) {
-	let [upper, dist] = [path[i]["upper-bound"], path[i]["distance"]];
+	let [upper, _] = [path[i]["upper-bound"], path[i]["distance"]];
 
-	console.log(booklocs.find(code => lc.lt(code, upper) && lc.gt(code, lower)), "a")
+	let found = booklocs.find(code => lc.lt(code, upper) && lc.gt(code, lower));
+	if (found === undefined) {
+	    result.push(i)
+	} else {
+	}
     }
+    return result
 }
 
+// path: json from the database for a particular floor
+// skip_idx: index of path to skip
+//
+// Returns a new booklocs with that index skipped, and the distance updated
+function skip_shelf(path, skip_idx) {
+    // Clone the path
+    let result = path.map(x => x);
+    console.log(result, "a");
+    result[skip_idx-1]["distance"] += result[skip_idx]["distance"]
+    console.log(result, "b");
+    result.remove(skip_idx)
+    return result
+}
 
 let a = [
     {"upper-bound": "M7", "distance": 7}, 
@@ -16,4 +40,8 @@ let a = [
 
 let books = ["M2", "M9"];
 
-skip_shelves(a, "M1", books)
+console.log(keep_shelves_indexes(a, "M1", books))
+console.log(keep_shelves_indexes(a, "M5", books))
+skip_shelf(books, 1)
+console.log(a)
+
