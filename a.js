3c7a8d2a5ee991792876ace65bb2b31794ff53d2
Alex Dikelsky <alexander.dikelsky@gmail.com>
2021-11-16 14:49:27 -0600

    Add function for filtering elements

--- a.js
+++ a.js
@@ -7,29 +7,19 @@ const lc = require('lc_call_number_compare');
 // No mutation
 function keep_shelves_indexes(path, lower, booklocs) {
     let result = [];
+    let cost = 0;
     for (let i = 0; i < path.length; i++) {
-	let [upper, _] = [path[i]["upper-bound"], path[i]["distance"]];
+	let [upper, cost_here] = [path[i]["upper-bound"], path[i]["distance"]];
 
+	cost += cost_here;
 	let found = booklocs.find(code => lc.lt(code, upper) && lc.gt(code, lower));
-	if (found === undefined) {
-	    result.push(i)
-	} else {
+
+	// If it is found, add it
+	if (found !== undefined) {
+	    result.push([i, cost]);
+	    cost = 0;
 	}
     }
-    return result
-}
-
-// path: json from the database for a particular floor
-// skip_idx: index of path to skip
-//
-// Returns a new booklocs with that index skipped, and the distance updated
-function skip_shelf(path, skip_idx) {
-    // Clone the path
-    let result = path.map(x => x);
-    console.log(result, "a");
-    result[skip_idx-1]["distance"] += result[skip_idx]["distance"]
-    console.log(result, "b");
-    result.remove(skip_idx)
     return result
 }
 
@@ -42,6 +32,7 @@ let books = ["M2", "M9"];
 
 console.log(keep_shelves_indexes(a, "M1", books))
 console.log(keep_shelves_indexes(a, "M5", books))
-skip_shelf(books, 1)
+// console.log(skip_shelf(books, 1))
+// console.log(skip_shelf(books, 0))
 console.log(a)
 
