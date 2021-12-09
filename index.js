a5212bc8474ccd96940593fb324113fe5bb79b29
Isaac List <listis01@luther.edu>
2021-12-08 21:57:58 -0600

    possible fix to SQL query, needs testing

--- index.js
+++ index.js
@@ -124,12 +124,11 @@ app.post('/add', async (req, res) => {
 
     // Add book info (from OCLC response) to Database
     const client = await pool.connect();
-    const text = `INSERT INTO booklist(isbn, author, title, call_no) VALUES(
-      "${book.isbn}", "${book.author}", "${book.title}", "${book.call_no}"
-      )`;
+    const text = "INSERT INTO booklist(isbn, author, title, call_no) VALUES($1, $2, $3, $4) RETURNING *"
+    const values = [book.isbn, book.author, book.title, book.call_no];
   
     try {
-      const res = await client.query(text)
+      const res = await client.query(text, values)
       console.log(res.rows[0])
     } catch (err) {
       console.log(err.stack)
